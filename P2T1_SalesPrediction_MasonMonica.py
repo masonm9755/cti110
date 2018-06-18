dde6491b60649c380f25fdc7638a537a58d89df2
masonm9755 <40346466+masonm9755@users.noreply.github.com>
2018-06-17 22:03:28 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_MasonMonica.py
@@ -0,0 +1,8 @@
+# CTI-110
+# P2T1 - Sales Prediction
+# Monica Mason
+# June 14, 2018
+#
+totalSales = float(input('Enter the projected amount of total sales: '))
+annualProfit = 0.23 * totalSales
+print(annualProfit)
